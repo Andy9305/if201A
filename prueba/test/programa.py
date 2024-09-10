52ddbe3e9322fabd276383381181f3b495c585b4
Andy9305 <202411438@urp.edu.pe>
2024-09-10 19:26:26 +0000

    herencia vehiculo

--- prueba/test/programa.py
+++ prueba/test/programa.py
@@ -7,3 +7,7 @@ def suma(x, y):
 
 def es_mayor(x, y):
     return (x>y)
+def divide(x, y):
+    if y== 0:
+        return ValueError("No se puede dividir por 0")
+    return(x/y)
--- prueba/test/test_programa.py
+++ prueba/test/test_programa.py
@@ -3,7 +3,7 @@ Modulo de pruebas unitarias para programa.py
 Fecha:09/09/2024
 '''
 import unittest
-from programa import suma, es_mayor
+from programa import suma, es_mayor, divide
 
 class Testprograma(unittest.TestCase):
     # Metodos de prueba
@@ -16,12 +16,20 @@ class Testprograma(unittest.TestCase):
     def test_suma_dentro_de_un_rango(self):
         self.assertIn(suma(2,5),[1, 2, 4, 6, 7])
 # Desarrollar pruebas para la funcion "es_mayor"
+
     def test_verifica_si_es_mayor(self):
         self.assertTrue(es_mayor(10,5))
     def test_verifica_que_no_es_mayor(self):
         self.assertFalse(es_mayor(2,8))
 
 # Desarrollar pruebas para la division de 2 numeros
+
+    def test_division_de_positivo_con_negativos(self):
+        self.assertEqual(divide(10,-2), -5)
+        self.assertEqual(divide(-4, 2), -2)
+    def test_division_por_cero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
 
 if __name__ =='__main__':
     unittest.main()
